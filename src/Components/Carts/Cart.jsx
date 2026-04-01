b4fe82b6c802890193524239cf604c3742015f72
Mahadi176 <mahadi210176@gmail.com>
2026-04-01 22:47:01 +0600

    cart updated

--- src/Components/Carts/Cart.jsx
+++ src/Components/Carts/Cart.jsx
@@ -18,30 +18,28 @@ const Cart = ({carts,setCarts}) => {
                 
                 <div className='w-full'>
                 {
-                    carts.map(cart => <div key={cart.id} className='md:flex items-center justify-between border border-gray-100 shadow-lg rounded-4xl p-5 mb-5  bg-gray-200'>
-                        <div className=''>
-                            <img className='h-20 w-20' src={cart.image} alt="" />
+                    carts.map(cart => <div key={cart.id} className='md:flex items-center justify-between border border-gray-100 shadow-lg rounded-4xl p-5 mb-5  bg-gray-100'>
+                       <div className='flex items-center justify-center gap-5'>
+                         <div className=''>
+                            <img className='h-10 w-10' src={cart.image} alt="" />
                         </div>
                         <div>
                             <h2 className='text-xl font-bold'>{cart.title}</h2>
-                            <p>{cart.description}</p>
+                            <p>${cart.price}</p>
                         </div>
+                       </div>
                         <div>
-                            <h2 className='text-2xl font-bold'>{cart.price}</h2>
-                            <p>/Month</p>
-                        </div>
-                        <div>
-                            <button onClick={() => handleDelete(cart)} className='btn rounded-full bg-violet-700 text-white'>X</button>
+                            <div onClick={() => handleDelete(cart)} className=' text-red-500'>Remove</div>
                         </div>
                     </div>)
                 }
             </div>
-            <div className='flex w-full items-center justify-between px-7 py-10 mt-5  bg-black text-white text-3xl font-bold rounded-4xl '>
+            <div className='flex w-full items-center justify-between px-7 py-10 mt-5 '>
                 <div>Total</div>
-                <div>$ {totalPrice}</div>
+                <div className='text-3xl font-bold'>$ {totalPrice}</div>
             </div>
             <div>
-                <button onClick={handleCheckout} className='w-full bg-red-500 text-white text-2xl font-bold p-5 rounded-4xl my-5'>
+                <button onClick={handleCheckout} className='w-full bg-violet-700 text-white text-2xl font-bold p-5 rounded-4xl my-5'>
                     checkout
                 </button>
             </div>
--- src/Components/Products/ProductCard.jsx
+++ src/Components/Products/ProductCard.jsx
@@ -12,7 +12,7 @@ const ProductCard = ({product,carts,setCarts}) => {
     }
 
     return (
-        <div className='max-w-80 p-4 border border-zinc-200 shadow-lg rounded-2xl'>
+        <div className='max-w-80 p-4 border border-zinc-200 shadow-lg rounded-2xl flex flex-col'>
             {
               product.status === "best-seller" &&   <div className='flex justify-end'>
               <p className='p-1 bg-yellow-200 text-amber-900 w-25 rounded-full text-sm text-center'>Best Seller</p>
@@ -29,21 +29,24 @@ const ProductCard = ({product,carts,setCarts}) => {
           </div>
             }
         
-            <img className='bg-base-200 rounded-full p-2 mb-4' src={product.image} alt="" />
-            <h1 className='text-2xl font-bold mb-4'>{product.title}</h1>
+            <div>
+                <img className='bg-base-200 rounded-full p-2 mb-4 h-10 w-10' src={product.image} alt="" />
+            </div>
+            <h1 className='text-2xl font-bold mb-4 flex-1'>{product.title}</h1>
             <p>{product.description}</p>
-            <div className='flex items-center gap-2 mb-4'>
+            <div className='flex items-center gap-2 my-4'>
                 <h1 className='text-xl font-bold'>${product.price}</h1>
                 <p>/Month</p>
             </div>
-            <div>
+            <div className='mb-5'>
                 {
                     product.features.map((feature,index)=> <ProductFeature key={index} feature={feature}/>)
                 }
             </div>
-            <button onClick={handleBuy} className='btn '>{isBuy ?  "Added to Cart !" : "Buy Now"}</button>
+            <button onClick={handleBuy} className={isBuy ? " btn bg-green-600 w-full text-white px-4 py-2 rounded" : "btn bg-violet-700 text-white px-4 py-2 rounded w-full"}
+            >{isBuy ? "Added to Cart !" : "Buy Now"}</button>
         </div>
     );
 };
 
-export default ProductCard;
+export default ProductCard; 
