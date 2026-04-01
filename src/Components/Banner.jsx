6f56c55e3abac9d325c85a01d3e3db6fbb9609cb
Mahadi176 <mahadi210176@gmail.com>
2026-04-01 13:42:13 +0600

    banner added

---
+++ src/Components/Banner.jsx
@@ -0,0 +1,44 @@
+import { Target } from 'lucide-react';
+import { Play } from 'lucide-react';
+import React from 'react';
+
+const Banner = () => {
+  return (
+    <div className="relative min-h-162.5 flex items-center overflow-hidden">
+      <div className=" mx-auto grid grid-cols-1 lg:grid-cols-2 gap-10 items-center relative z-10">
+        {/* Left Content */}
+        <div className="space-y-4">
+          <div className="inline-flex items-center gap-2 bg-violet-500/10 border border-violet-500/30 text-violet-600 text-sm font-medium px-5 py-2 rounded-full">
+           <Target/>   New Ai-Powered Tools Available
+          </div>
+
+          <h1 className="text-4xl lg:text-5xl font-bold leading-tight tracking-tighter">
+           Supercharge Your <br />Digital Workflow
+          </h1>
+
+          <p className="text-lg text-zinc-700 max-w-lg">
+        Access premium AI tools, design assets, templates, and productivity software—all in one place. Start creating faster today. Explore Products
+          </p>
+
+          <div className="flex flex-wrap gap-4 pt-4">
+            <button className='btn bg-violet-700 text-white rounded-4xl'>Explore Products</button>
+            <button className='btn border border-violet-600 text-violet-600 rounded-4xl'><Play /> Watch Demo</button>
+          </div>
+        </div>
+
+        {/* Right Image */}
+        <div className="relative flex justify-center lg:justify-end">
+          <div className="relative">
+            <img
+              className="relative h-130 w-auto object-contain drop-shadow-2xl"
+              src="/src/assets/banner.png"
+              alt="AI Models Banner"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <div className="md:flex items-center justify-between shadow-sm bg-base-100 py-5 md:px-30">
       <div >
-        <h1 className='text-3xl font-bold text-violet-600 text-center py-2'>DigiTools</h1>
+        <h1 className='text-3xl font-bold text-violet-700 text-center py-2'>DigiTools</h1>
       </div>
       <div className=" hidden md:flex">
         <ul className="menu menu-horizontal gap-10 px-1 text-lg">
