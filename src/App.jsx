78f6db5d9eeabd6123505379ef21f0f59e466058
Mahadi176 <mahadi210176@gmail.com>
2026-04-01 17:14:13 +0600

    pricing card added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,16 @@ import Banner from './Components/Banner'
 import Banner2 from './Components/Banner2'
 import Footer from './Components/Footer'
 import GetStarted from './Components/GetStarted'
+import Pricing from './Components/Pricing/Pricing'
+import { Suspense } from 'react'
+
+const getPricing = async() => {
+  const res = await fetch('../public/Pricing.json');
+  return res.json();
+}
+
+const pricingPromise = getPricing();
+
 
 function App() {
  
@@ -13,6 +23,9 @@ function App() {
       <Banner/>
       <Banner2/>
       <GetStarted/>
+     <Suspense fallback={<span class="loading loading-spinner loading-xl"></span>}>
+       <Pricing pricingPromise={pricingPromise}/>
+     </Suspense>
       <Footer/>
     </>
   )
---
+++ src/Components/Pricing/Pricing.jsx
@@ -0,0 +1,22 @@
+import { use } from "react";
+import PricingCard from "./PricingCard";
+
+const Pricing = ({pricingPromise}) => {
+    const pricingData = use(pricingPromise)
+   
+    return (
+        <div className="md:p-20 max-w-7xl mx-auto  ">
+       <div className="mx-auto text-center">
+           <h1 className="text-5xl font-bold">Simple, Transparent Pricing</h1>
+           <p className="my-5">Choose the plan that fits your needs. Upgrade or downgrade anytime.</p>
+        </div>
+        <div className="md:flex justify-center gap-10 p-4">
+            {
+                pricingData.map(price =><PricingCard key={price.id} price={price}/>)
+            }
+        </div>
+        </div>
+    );
+};
+
+export default Pricing;
